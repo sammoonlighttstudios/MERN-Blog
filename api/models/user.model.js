af9eb928279b8487548eca8117e5346442bd68b1
sammoonlighttstudios <165544338+sammoonlighttstudios@users.noreply.github.com>
2024-05-11 19:05:04 +0530

    updated header component wiith profile pic

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,8 +17,8 @@ password:{
 },
 profilePicture:{
   type: String,
-  default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-}
+  default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+},
 },{timestamps: true}
 );
 const User = mongoose.model("User", userSchema);
--- mern blog project/src/components/Header.jsx
+++ mern blog project/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 function Header() {
-  const pathname = useLocation().pathname
+const pathname = useLocation().pathname;
+const {Currentuser}= useSelector(state => state.user);
+ console.log(Currentuser);
   return (
-   
+
     <Navbar className='border-b-2'>
+
       <Link to="/" className='self-center widespace-nowrap text-sm sm:text-xl fond-semibold  dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-r from-blue-400 to-violet-500 text-white rounded-lg'>SAM</span>
         <span>Blog</span>
@@ -34,35 +38,60 @@ function Header() {
           <FaMoon />
         </Button>
 
+     {Currentuser ? (
+      <Dropdown
+       arrowIcon={false}
+       inline
+       label={<Avatar alt="user" img={Currentuser.profilePicture} rounded />}>
+       <Dropdown.Header>
+       <span className='block text-sm truncate'>@{Currentuser.username}</span>
+       <span className='block text-sm font-medium truncate'>{Currentuser.email}</span>
+             
+       </Dropdown.Header>
+       <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+            
+       </Dropdown>
+     ):
+     (
+      <Link to='/signin'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+     )}
 
-        <Link to="/signin">
-          <Button className='border-0' gradientDuoTone="purpleToBlue" outline>
-            Signin
-          </Button>
-        </Link>
+
+
+
+
+
         <Navbar.Toggle />
-      </div>
+      </div >
       <Navbar.Collapse>
-        <Navbar.Link active={pathname==="/"} as={"div"}>
+        <Navbar.Link active={pathname === "/"} as={"div"}>
           <Link to="/">
             Home
           </Link>
 
         </Navbar.Link>
-        <Navbar.Link active={pathname==="/about"} as={"div"}>
+        <Navbar.Link active={pathname === "/about"} as={"div"}>
           <Link to="/about">
             About
           </Link>
 
         </Navbar.Link>
-        <Navbar.Link active={pathname==="/project"} as={"div"}>
+        <Navbar.Link active={pathname === "/project"} as={"div"}>
           <Link to="/project">
             Project
           </Link>
 
         </Navbar.Link>
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar >
 
   )
 }
