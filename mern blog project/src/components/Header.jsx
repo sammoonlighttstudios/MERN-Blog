f30d815d22295c217d2998f42964fe124e0dc7b3
sammoonlighttstudios <165544338+sammoonlighttstudios@users.noreply.github.com>
2024-05-16 15:42:25 +0530

    add darkmode funtionality

--- mern blog project/src/components/Header.jsx
+++ mern blog project/src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import { FaMoon,FaSun} from 'react-icons/fa'
+import { useSelector,useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 function Header() {
 const pathname = useLocation().pathname;
+const dispatch = useDispatch();
 const {Currentuser}= useSelector(state => state.user);
+const {theme} = useSelector((state) => state.theme);
  console.log(Currentuser);
   return (
 
@@ -34,8 +37,9 @@ const {Currentuser}= useSelector(state => state.user);
       </Button>
 
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color="gray" pill>
-          <FaMoon />
+        <Button className='w-12 h-10 hidden sm:inline' color="gray" pill onClick={()=>dispatch(toggleTheme())}>
+          {theme === 'light' ? <FaMoon/> : <FaSun/>}
+       
         </Button>
 
      {Currentuser ? (
