0d2e0d55bf01a8574182d782c826eb81afda9539
sammoonlighttstudios <165544338+sammoonlighttstudios@users.noreply.github.com>
2024-05-17 16:48:56 +0530

    make dashboard private

--- mern blog project/src/App.jsx
+++ mern blog project/src/App.jsx
@@ -4,9 +4,12 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Project from "./pages/Project"
 import Dashboard from "./pages/Dashboard"
+
 import SignUp from "./pages/SignUp"
 import Signin from "./pages/Signin"
 import Footer from "./components/footer"
+import PrivateRoute from "./components/PrivateRoute"
+
 
 
 
@@ -19,7 +22,10 @@ export default function App() {
        <Route path="/" element={<Home />}/>
        <Route path="/about" element={<About />}/>
        <Route path="/project" element={<Project />}/>
+       <Route element={<PrivateRoute />}>
        <Route path="/dashboard" element={<Dashboard />}/>
+       </Route>
+
        <Route path="/signup" element={<SignUp />}/>
        <Route path="/signin" element={<Signin />}/>
 
